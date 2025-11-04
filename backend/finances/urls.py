4e014e2c31546220dd371978c95fef676e28e0ff
deffin05 <dm.melnyk05@gmail.com>
2025-11-04 20:21:14 +0200

    Create simple views for existing resources

--- backend/finances/urls.py
+++ backend/finances/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from finances.views import TransactionList, TransactionDetail, BalanceDetail, BalanceCreate
+
 urlpatterns = [
+    path('transactions/', TransactionList.as_view()),
+    path('transactions/<int:pk>/', TransactionDetail.as_view()),
+    path('balance/', BalanceCreate.as_view()),
+    path('balance/<int:pk>/', BalanceDetail.as_view()),
 ]
