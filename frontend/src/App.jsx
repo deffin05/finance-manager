9892cc5d5b6ea2b33ffbb8e1aabcee33ae60a755
deffin05 <dm.melnyk05@gmail.com>
2025-11-11 18:26:15 +0200

    Make a simple page with a form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
+const backendUrl = 'http://localhost:8000/';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <TransactionForm />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </>
+  )
+}
+
+function TransactionForm() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = JSON.stringify(Object.fromEntries(new FormData(document.forms['transactionForm']).entries()));
+    fetch(backendUrl + 'transactions/',
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: data,
+      }).catch((error) => console.error(error));
+  }
+  return (
+    <>
+      <form id='transactionForm' style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', gap: 4 }} onSubmit={handleSubmit}>
+        <label form='user'>User id</label>
+        <input type='number' name='user' required />
+
+        <label form='amount'>Amount</label>
+        <input type='number' name='amount' step={0.01} required />
+
+        <label form='currency'>Currency</label>
+        <input type='text' name='currency' required minLength={3} maxLength={3} />
+
+        <input type='submit' />
+      </form>
     </>
   )
 }
