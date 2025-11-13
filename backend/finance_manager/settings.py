0cfa282d8438e02c24c45b677abde73c3f0f7c63
deffin05 <dm.melnyk05@gmail.com>
2025-11-13 10:32:22 +0200

    Add tests for auth

--- backend/finance_manager/settings.py
+++ backend/finance_manager/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'auth.apps.AuthConfig',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'drf_spectacular',
     'drf_spectacular_sidecar',
 ]
