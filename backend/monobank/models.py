a4caeaac56d683959501368b7f5aa6ada3eb325f
deffin05 <dm.melnyk05@gmail.com>
2025-12-12 22:02:45 +0200

    Add basic model for monobank user

--- backend/monobank/models.py
+++ backend/monobank/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class MonobankUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100)
