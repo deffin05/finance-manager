bbd7a602abfd81ee6bcaa3aab4f840d765d5ede0
deffin05 <dm.melnyk05@gmail.com>
2025-12-13 10:13:13 +0200

    Change Transaction model to allow setting custom date

--- backend/finances/models.py
+++ backend/finances/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -13,7 +14,7 @@ CURRENCIES = {
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
     amount = models.DecimalField(decimal_places=2, max_digits=20)
     currency = models.CharField(max_length=3, choices=CURRENCIES.items())
 
